173f86a84fd625fe67ae4b78667b049c6542ab71
Diego <godiegosaenz@hotmail.com>
2018-12-24 14:00:33 -0500

    Se creo el login

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -4,7 +4,7 @@
             <div class="col-md-8">
                 <div class="card card-default">
                     <div class="card-header">Example Component</div>
-
+                    <b-alert variant="success" show>Success Alert</b-alert>
                     <div class="card-body">
                         I'm an example component.
                     </div>
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+<b-container>
+    <b-row align-h="center">
+        <b-col cols="8">
+            <b-card header="featured"
+                header-tag="header"
+                footer="Card Footer"
+                footer-tag="footer"
+                title="Title">
+                <b-alert variant="success" show>Success Alert</b-alert>
+                <b-form>
+
+                    <b-form-group id="exampleInputGroup1"
+                                    label="Correo:"
+                                    label-for="email"
+                                    description="Tu correo esta seguro">
+                        <b-form-input id="email"
+                                    type="email"
+                                    name="email"
+                                    required
+                                    value="{{old('email')}}"
+                                    autofocus
+                                    placeholder="Correo">
+                        </b-form-input>
+                    </b-form-group>
+                    <b-form-group id="exampleInputGroup1"
+                                    label="Contraseña:"
+                                    label-for="password"
+                                    description="Tu correo estara segura">
+                        <b-form-input id="password"
+                                    type="password"
+                                    name="password"
+                                    required
+                                    value="{{old('password')}}"
+                                    autofocus
+                                    placeholder="Ingrese clave aqui">
+                        </b-form-input>
+                    </b-form-group>
+                    <b-form-group>
+                        <b-form-checkbox {{ old('remember') ? 'checked="true"' : ''}}
+                                        id="remember"
+                                        name="remember"
+                                        checked>
+                        Recordar sesion
+                        </b-form-checkbox>
+                    </b-form-group>
+                    <b-button
+                            type="submit"
+                            variant="primary">
+                        Ingresar
+                    </b-button>
+                    <b-button variant="link">
+                        ¿Olvidaste tu contraseña?
+                    </b-button>
+
+                </b-form>
+            </b-card>
+        </b-col>
+    </b-row>
+</b-container>
+@endsection
